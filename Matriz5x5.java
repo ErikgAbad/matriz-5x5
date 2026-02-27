9bc2ee868b4c3a5b3e70a0c56834fc52cfdb2c4e
ErikgAbad <ec.abadab@uea.edu.ec>
2026-02-26 20:51:44 -0500

    Tarea matriz 5x5

---
+++ Matriz5x5.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ERIKG
+ */
+import java.util.Scanner;
+
+public class Matriz5x5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matriz = new int[5][5];
+
+        // Ingreso de datos por consola
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("Ingrese el valor para la posición [" + i + "][" + j + "]: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        // Mostrar matriz
+        System.out.println("\nMatriz ingresada:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
---
+++ Tarea/Tarea_S6.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tarea;
+
+/**
+ *
+ * @author Erikg
+ */
+public class Tarea_S6 {
+    public static void main(String[] args) {
+        int edad=12;
+        boolean tieneDocumento=true;
+        if(edad>=18 && tieneDocumento==true ){ 
+            System.out.println("Puede acceder al servicio");
+        }
+        else{
+            System.out.println(" No puede acceder al servicio");
+        }
+    }
+    
+}
